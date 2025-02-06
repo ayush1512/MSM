68be0cf825ede853eae350efcc5c7c4f0db6f7e1
Saurabh Dwivedi <saurabhdwivedi01513@gmail.com>
2025-02-07 05:15:30 +0530

    commit

--- WebTestScanner/app.py
+++ WebTestScanner/app.py
@@ -153,5 +153,6 @@ def process_image():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
-    app.run(host="0.0.0.0", port=port, debug=True)
+    # port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
+    app.run(host="0.0.0.0", debug=True)
+
