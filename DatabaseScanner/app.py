a649a8e55d90eda97e81262dc46726e3c5d8d414
ayush1512 <ayushsaxena1512@gmail.com>
2025-03-03 09:52:11 +0530

    added quantity

--- DatabaseScanner/app.py
+++ DatabaseScanner/app.py
@@ -374,7 +374,7 @@ def process_image():
 def update_stock():
     try:
         data = request.json
-        required_fields = ['medicine_id', 'batch_no', 'mfg_date', 'exp_date', 'mrp', 'image_url']
+        required_fields = ['medicine_id', 'batch_no', 'mfg_date', 'exp_date', 'mrp', 'image_url', 'quantity']
         
         if not all(field in data for field in required_fields):
             return jsonify({"error": "Missing required fields"}), 400
@@ -393,6 +393,7 @@ def update_stock():
                     'mfg_date': data['mfg_date'],
                     'exp_date': data['exp_date'],
                     'mrp': float(data['mrp']),
+                    'quantity': int(data['quantity']),
                     'image_url': data['image_url']
                 }}
             )
@@ -405,7 +406,8 @@ def update_stock():
                 mfg_date=data['mfg_date'],
                 exp_date=data['exp_date'],
                 mrp=float(data['mrp']),
-                image_url=data['image_url']
+                image_url=data['image_url'],
+                quantity=int(data['quantity'])
             )
             result = stock_collection.insert_one(stock.to_dict())
             message = 'New stock entry created successfully'
