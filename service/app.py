af3cbfc249188311f11b5b596603e3b68bbae3d1
ayush1512 <ayushsaxena1512@gmail.com>
2025-04-08 02:26:30 +0530

    updated service/app.py

--- service/app.py
+++ service/app.py
@@ -100,12 +100,12 @@ def search_medicine():
                 {'product_manufactured': {'$regex': search_term, '$options': 'i'}}
             ]
         }))
-        medicines = list(stock_collection.find({
-            '$or': [
-                {'product_name': {'$regex': search_term, '$options': 'i'}},
-                {'product_manufactured': {'$regex': search_term, '$options': 'i'}}
-            ]
-        }))
+        # medicines = list(stock_collection.find({
+        #     '$or': [
+        #         {'product_name': {'$regex': search_term, '$options': 'i'}},
+        #         {'product_manufactured': {'$regex': search_term, '$options': 'i'}}
+        #     ]
+        # }))
 
 
         # Convert ObjectId to string for JSON serialization
