dc6ef4f43a3350d84cad516538ec58d3e975fe3d
ayush1512 <ayushsaxena1512@gmail.com>
2025-04-17 16:52:37 +0530

    update req & script

--- start_dev.sh
+++ start_dev.sh
@@ -18,7 +18,7 @@ fi
 echo "Starting Flask backend..."
 cd service
 source ../venv/Scripts/activate
-flask run --host='0.0.0.0' --port=5000 &
+flask run --host='0.0.0.0' --port=5000 --debug &
 cd ..
 
 # Wait for backend to start
